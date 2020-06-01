0713e9b385bb120fa783043aa5f8a7768a74678b
HubNeu <hub.neubauer@gmail.com>
2020-06-01 23:03:17 +0200

    all the files in one commit

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class UiManager : MonoBehaviour {
+
+	int score = 0;
+	public Text stext;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void IncrementScore(){
+		score++;
+		if (score==15) {
+			Time.timeScale = 0; 
+			stext.text = "You win!";
+		}
+	}
+	
+	public void loose(){
+		Time.timeScale = 0; 
+		stext.text = "Game over!";
+	}
+}
